d3e12553207f2ce7bed23bb6be343d0f661340ec
USERSAD\jhenriqa <joao.henrique.anjos@everis.com>
2023-05-03 10:56:35 +0100

    Step 3 : Get configuration from application properties

--- limits-service/src/main/java/com/in28minutes/rest/service/limitsservice/controller/LimitsController.java
+++ limits-service/src/main/java/com/in28minutes/rest/service/limitsservice/controller/LimitsController.java
@@ -1,16 +1,21 @@
 package com.in28minutes.rest.service.limitsservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.in28minutes.rest.service.limitsservice.configuration.Configuration;
 import com.in28minutes.rest.service.limitsservice.domain.Limits;
 
 @RestController
 public class LimitsController {
-
+	
+	@Autowired
+	private Configuration configuration;
 	
 	@GetMapping("/limits")
 	public Limits retrieveLimits() {
-		return new Limits(1, 1000);
+		return new Limits(configuration.getMinimum(), configuration.getMaximum());
+		//return new Limits(1, 1000);
 	}
 }
